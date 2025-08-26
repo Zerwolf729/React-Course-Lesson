ba14f17a3453d553b1c0a46d6e5d6d820079c653
Samuel <samueljuliank29@gmail.com>
2025-08-26 19:52:15 +0800

    Finish React Lesson Part 7

--- ecommerce-project/src/pages/checkout/CheckoutHeader.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutHeader.jsx
@@ -1,10 +1,16 @@
-import { Link } from 'react-router'
-import CheckoutLockIcon from '../../assets/images/icons/checkout-lock-icon.png';
-import Logo from '../../assets/images/logo.png';
-import MobileLogo from '../../assets/images/mobile-logo.png';
-import './CheckoutHeader.css';
+import { Link } from "react-router";
+import CheckoutLockIcon from "../../assets/images/icons/checkout-lock-icon.png";
+import Logo from "../../assets/images/logo.png";
+import MobileLogo from "../../assets/images/mobile-logo.png";
+import "./CheckoutHeader.css";
 
-const CheckoutHeader = () => {
+const CheckoutHeader = ({ cart }) => {
+  let totalQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    totalQuantity += cartItem.quantity;
+  });
+
   return (
     <div className="checkout-header">
       <div className="header-content">
@@ -18,7 +24,7 @@ const CheckoutHeader = () => {
         <div className="checkout-header-middle-section">
           Checkout (
           <Link className="return-to-home-link" to="/">
-            3 items
+            {totalQuantity} items
           </Link>
           )
         </div>
--- ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -26,7 +26,7 @@ const CheckoutPage = ({ cart }) => {
 
       <link rel="icon" type="image/svg+xml" href="/images/cart-favicon.png" />
 
-      <CheckoutHeader />
+      <CheckoutHeader cart={cart} />
 
       <div className="checkout-page">
         <div className="page-title">Review your order</div>
